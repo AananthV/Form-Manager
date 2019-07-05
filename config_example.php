f1e54ad34f32948bfb70ccaa5ab4e3d8bd917cbe
Aananth V <aananthrox@gmail.com>
2019-07-05 12:53:31 +0530

    Add instructions in config_example.php

--- config_example.php
+++ config_example.php
@@ -1,5 +1,14 @@
 <?php
-  define('DOMAIN', 'localhost');
+  /*
+  * The domain where the Form Builder is hosted.
+  * It must end in a forward slash '/'
+  * 'http://www.example.com/' is valid while 'http://www.example.com' is not.
+  */
+  define('DOMAIN', 'http://localhost/');
+
+  /*
+  * The MYSQL database configuration.
+  */
   define('DB_HOST', 'localhost');
   define('DB_NAME', 'form_manager');
   define('DB_USERNAME', 'root');
